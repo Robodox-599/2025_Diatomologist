539c47100b85fefcea2265457a49e80624725f20
litlizlovesprog <lizzzz87658765@gmail.com>
2025-01-16 16:50:27 -0800

    Deleted claw & wrote RobotContainer

--- 2025_NoName/src/main/java/frc/robot/RobotContainer.java
+++ 2025_NoName/src/main/java/frc/robot/RobotContainer.java
@@ -4,15 +4,57 @@
 
 package frc.robot;
 
+//import com.ctre.phoenix6.hardware.TalonFX;
+
+import dev.doglog.DogLog;
+import dev.doglog.DogLogOptions;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+
+import frc.robot.endefector.rollers.Rollers;
+import frc.robot.endefector.rollers.RollersIOSim;
+import frc.robot.endefector.rollers.RollersIOTalonFX;
+
+import frc.robot.endefector.wrist.Wrist;
+import frc.robot.endefector.wrist.WristIOSim;
+import frc.robot.endefector.wrist.WristIOTalonFX;
 
 public class RobotContainer {
+  private Rollers rollers;
+  private Wrist wrist;
+
+  private final CommandXboxController controller =
+  new CommandXboxController(Constants.driverControllerPort);
+
   public RobotContainer() {
+    switch (Constants.currentMode) {
+      case REAL:
+        rollers = new Rollers(new RollersIOTalonFX());
+        wrist = new Wrist(new WristIOTalonFX());
+
+        break;
+      case SIM:
+        rollers = new Rollers(new RollersIOSim());
+        wrist = new Wrist(new WristIOSim());
+       
+        break;
+    }
+
+      DogLog.setOptions(
+        new DogLogOptions().withCaptureDs(true).withCaptureNt(true).withNtPublish(true));
+
     configureBindings();
   }
+  
 
-  private void configureBindings() {}
+  private void configureBindings() {
+    controller.x().onFalse(wrist.stop());
+    controller.a().onFalse(rollers.stop());
+
+    controller.x().whileTrue(wrist.goToPose(0));
+    controller.a().whileTrue(rollers.setVelocity(0));
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");

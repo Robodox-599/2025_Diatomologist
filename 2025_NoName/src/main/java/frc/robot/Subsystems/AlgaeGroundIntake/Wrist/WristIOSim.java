059f45bec51191d96a2de66025ef210cddd71947
PaullyPocket <michaelpaulb09@gmail.com>
2025-01-16 17:03:44 -0800

    Wrist update
    Wrist is basically done. Just got to correct some stuff.

--- 2025_NoName/src/main/java/frc/robot/Subsystems/AlgaeGroundIntake/Wrist/WristIOSim.java
+++ 2025_NoName/src/main/java/frc/robot/Subsystems/AlgaeGroundIntake/Wrist/WristIOSim.java
@@ -1,5 +1,22 @@
 package frc.robot.subsystems.algaegroundintake.wrist;
 
-public class WristIOSim {
-    
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
+
+public class WristIOSim implements WristIO {
+    private final DCMotor wristGearbox = DCMotor.getKrakenX60Foc(1);
+
+    private ProfiledPIDController m_controller;
+    private SimpleMotorFeedforward m_Feedforward = new  SimpleMotorFeedforward(0, 0);
+
+    private SingleJointedArmSim sim =
+        new SingleJointedArmSim(wristGearbox, WristConstants.IntakeWristSimConstants.kArmReduction, SingleJointedArmSim.estimateMOI(WristConstants.IntakeWristSimConstants.kArmLength,
+        WristConstants.IntakeWristSimConstants.kArmMass),
+        WristConstants.IntakeWristSimConstants.kArmLength,
+        WristConstants.IntakeWristSimConstants.kMinAngleRads,
+        WristConstants.IntakeWristSimConstants.kMaxAngleRads,
+        true,
+        0.1);
 }

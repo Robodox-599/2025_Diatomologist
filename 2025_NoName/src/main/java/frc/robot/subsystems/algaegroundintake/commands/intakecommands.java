26f2f3ef5c6da11d5199a24f1f7767d9a19f398c
litlizlovesprog <lizzzz87658765@gmail.com>
2025-01-29 17:03:29 -0800

    Commented out intakeCommands

--- 2025_NoName/src/main/java/frc/robot/subsystems/algaegroundintake/commands/intakecommands.java
+++ 2025_NoName/src/main/java/frc/robot/subsystems/algaegroundintake/commands/intakecommands.java
@@ -1,16 +1,16 @@
-package frc.robot.subsystems.algaegroundintake.commands;
+// package frc.robot.subsystems.algaegroundintake.commands;
 
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
-import frc.robot.subsystems.algaegroundintake.rollers.intakeRollers;
-import frc.robot.subsystems.algaegroundintake.wrist.intakeWrist;
+// import edu.wpi.first.wpilibj2.command.Command;
+// import edu.wpi.first.wpilibj2.command.Commands;
+// import frc.robot.subsystems.algaegroundintake.rollers.intakeRollers;
+// import frc.robot.subsystems.algaegroundintake.wrist.intakeWrist;
 
-public class intakecommands extends Command {
-    public static Command intakeDeployAndIntake(intakeWrist wrist, intakeRollers rollers) {
-        return Commands.parallel(
-            (wrist),
-            rollers.setVoltage(2));
+// public class intakecommands extends Command {
+//     public static Command intakeDeployAndIntake(intakeWrist wrist, intakeRollers rollers) {
+//         return Commands.parallel(
+//             (wrist),
+//             rollers.setVoltage(2));
 
 
-    }
-}
+//     }
+// }

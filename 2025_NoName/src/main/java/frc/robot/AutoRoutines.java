52e3aee291ee0e8c9a14768e43fadd6db64bfbc9
jaidenlikesmeer <jaidenyummy123@gmail.com>
2025-03-03 16:43:13 -0800

    programmed more autos and added commands
    yay :D

--- 2025_NoName/src/main/java/frc/robot/AutoRoutines.java
+++ 2025_NoName/src/main/java/frc/robot/AutoRoutines.java
@@ -4,13 +4,55 @@ import choreo.auto.AutoFactory;
 import choreo.auto.AutoRoutine;
 import choreo.auto.AutoTrajectory;
 import edu.wpi.first.wpilibj2.command.Commands;
-import frc.robot.subsystems.commands.SuperstructureCommands;
+import frc.robot.commands.SuperstructureCommands;
+import frc.robot.subsystems.elevator.ElevatorConstants;
 
 public class AutoRoutines {
   private AutoFactory autoFactory;
+  private SuperstructureCommands superstructureCommands;
 
   public AutoRoutines(AutoFactory autoFactory, SuperstructureCommands superstructureCommands) {
     this.autoFactory = autoFactory;
+    this.superstructureCommands = superstructureCommands;
+  }
+
+  public AutoRoutine testingAutoRoutine() {
+    AutoRoutine routine = autoFactory.newRoutine("testingAuto");
+
+    // Load the routine's trajectories
+    AutoTrajectory LEFTtoI = routine.trajectory("LEFTtoI");
+    AutoTrajectory ItoHP = routine.trajectory("ItoHP");
+    AutoTrajectory HPtoL = routine.trajectory("HPtoL");
+    AutoTrajectory LtoHP = routine.trajectory("LtoHP");
+    AutoTrajectory HPtoK = routine.trajectory("HPtoK");
+
+    // When the routine begins, reset odometry and start the first trajectory to go to the reef
+    routine.active().onTrue(Commands.sequence(LEFTtoI.resetOdometry(), LEFTtoI.cmd()));
+
+    // // When the previous trajectory is done, score L3, then go to HP and run intake
+    // LEFTtoI.done()
+    //     .onTrue(
+    //         Commands.sequence(
+    //             superstructureCommands.scoring(ElevatorConstants.ElevatorStates.L3),
+    //             Commands.parallel(ItoHP.cmd(), superstructureCommands.stationIntake())));
+
+    // // When the previous trajectory is done, start the next trajectory to go to the reef
+    // ItoHP.done().onTrue(HPtoL.cmd());
+
+    // // When the previous trajectory is done, score L3, then go to HP and run intake
+    // HPtoL.done()
+    //     .onTrue(
+    //         Commands.sequence(
+    //             superstructureCommands.scoring(ElevatorConstants.ElevatorStates.L3),
+    //             Commands.parallel(LtoHP.cmd(), superstructureCommands.stationIntake())));
+
+    // // When the previous trajectory is done, start the next trajectory to go to the reef
+    // LtoHP.done().onTrue(HPtoK.cmd());
+
+    // // When the previous trajectory is done, score L3
+    // HPtoK.done().onTrue(superstructureCommands.scoring(ElevatorConstants.ElevatorStates.L3));
+
+    return routine;
   }
 
   public AutoRoutine leftAutoRoutine() {
@@ -23,14 +65,31 @@ public class AutoRoutines {
     AutoTrajectory LtoHP = routine.trajectory("LtoHP");
     AutoTrajectory HPtoK = routine.trajectory("HPtoK");
 
-    // When the routine begins, reset odometry and start the first trajectory
+    // When the routine begins, reset odometry and start the first trajectory to go to the reef
     routine.active().onTrue(Commands.sequence(LEFTtoI.resetOdometry(), LEFTtoI.cmd()));
 
-    // When the trajectory is done, start the next trajectories
-    // LEFTtoI.done().onTrue(ItoHP.cmd());
-    // ItoHP.done().onTrue(HPtoL.cmd());
-    // HPtoL.done().onTrue(LtoHP.cmd());
-    // LtoHP.done().onTrue(HPtoK.cmd());
+    // When the previous trajectory is done, score L3, then go to HP and run intake
+    LEFTtoI.done()
+        .onTrue(
+            Commands.sequence(
+                superstructureCommands.scoring(ElevatorConstants.ElevatorStates.L3),
+                Commands.parallel(ItoHP.cmd(), superstructureCommands.stationIntake())));
+
+    // When the previous trajectory is done, start the next trajectory to go to the reef
+    ItoHP.done().onTrue(HPtoL.cmd());
+
+    // When the previous trajectory is done, score L3, then go to HP and run intake
+    HPtoL.done()
+        .onTrue(
+            Commands.sequence(
+                superstructureCommands.scoring(ElevatorConstants.ElevatorStates.L3),
+                Commands.parallel(LtoHP.cmd(), superstructureCommands.stationIntake())));
+
+    // When the previous trajectory is done, start the next trajectory to go to the reef
+    LtoHP.done().onTrue(HPtoK.cmd());
+
+    // When the previous trajectory is done, score L3
+    HPtoK.done().onTrue(superstructureCommands.scoring(ElevatorConstants.ElevatorStates.L3));
 
     return routine;
   }
@@ -45,14 +104,31 @@ public class AutoRoutines {
     AutoTrajectory CtoHP = routine.trajectory("CtoHP");
     AutoTrajectory HPtoD = routine.trajectory("HPtoD");
 
-    // When the routine begins, reset odometry and start the first trajectory
+    // When the routine begins, reset odometry and start the first trajectory to go to the reef
     routine.active().onTrue(Commands.sequence(RIGHTtoF.resetOdometry(), RIGHTtoF.cmd()));
 
-    // When the trajectory is done, start the next trajectory
-    RIGHTtoF.done().onTrue(Commands.parallel(FtoHP.cmd()));
+    // When the previous trajectory is done, score L3, then go to HP and run intake
+    RIGHTtoF.done()
+        .onTrue(
+            Commands.sequence(
+                superstructureCommands.scoring(ElevatorConstants.ElevatorStates.L3),
+                Commands.parallel(FtoHP.cmd(), superstructureCommands.stationIntake())));
+
+    // When the previous trajectory is done, start the next trajectory to go to the reef
     FtoHP.done().onTrue(HPtoC.cmd());
-    HPtoC.done().onTrue(CtoHP.cmd());
+
+    // When the previous trajectory is done, score L3, then go to HP and run intake
+    HPtoC.done()
+        .onTrue(
+            Commands.sequence(
+                superstructureCommands.scoring(ElevatorConstants.ElevatorStates.L3),
+                Commands.parallel(CtoHP.cmd(), superstructureCommands.stationIntake())));
+
+    // When the previous trajectory is done, start the next trajectory to go to the reef
     CtoHP.done().onTrue(HPtoD.cmd());
+
+    // When the previous trajectory is done, score L3
+    HPtoD.done().onTrue(superstructureCommands.scoring(ElevatorConstants.ElevatorStates.L3));
 
     return routine;
   }
@@ -62,9 +138,20 @@ public class AutoRoutines {
 
     // Load the routine's trajectories
     AutoTrajectory MIDtoG = routine.trajectory("MIDtoG");
+    AutoTrajectory GtoS4 = routine.trajectory("GtoS4");
+    AutoTrajectory S4toNET = routine.trajectory("S4toNET");
 
     // When the routine begins, reset odometry and start the first trajectory
     routine.active().onTrue(Commands.sequence(MIDtoG.resetOdometry(), MIDtoG.cmd()));
+
+    // When the previous routine is done, score L3, go to the S4, grab algae, and go to net
+    MIDtoG.done()
+        .onTrue(
+            Commands.sequence(
+                superstructureCommands.scoring(ElevatorConstants.ElevatorStates.L3),
+                GtoS4.cmd(),
+                superstructureCommands.algaeL2Intake(),
+                S4toNET.cmd()));
 
     return routine;
   }
--- 2025_NoName/src/main/java/frc/robot/RobotContainer.java
+++ 2025_NoName/src/main/java/frc/robot/RobotContainer.java
@@ -4,20 +4,16 @@ import choreo.auto.AutoChooser;
 import choreo.auto.AutoFactory;
 import dev.doglog.DogLog;
 import dev.doglog.DogLogOptions;
-import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
 import frc.robot.Constants.*;
-import frc.robot.subsystems.commands.SuperstructureCommands;
+import frc.robot.commands.SuperstructureCommands;
 import frc.robot.subsystems.drive.Drive;
 import frc.robot.subsystems.drive.GyroIO;
 import frc.robot.subsystems.drive.GyroIOPigeon2;
-import frc.robot.subsystems.drive.constants.RealConstants;
-import frc.robot.subsystems.elevator.ElevatorConstants.ElevatorStates;
 import frc.robot.subsystems.subsystemvisualizer.SubsystemVisualizer;
 
 public class RobotContainer {
@@ -61,7 +57,6 @@ public class RobotContainer {
         //         new VisionIOReal(RealConstants.camConstants, drive::getPose));
         autoFactory =
             new AutoFactory(drive::getPose, drive::resetPose, drive::followChoreoPath, true, drive);
-        // autoFactory.bind("intake", stationIntake()).bind("score", scoring(ElevatorStates.L4));
         break;
       case SIM:
         DriverStation.silenceJoystickConnectionWarning(true);
@@ -78,7 +73,6 @@ public class RobotContainer {
         //         new VisionIOSim(RealConstants.cam2Constants, drive::getPose));
         autoFactory =
             new AutoFactory(drive::getPose, drive::resetPose, drive::followChoreoPath, true, drive);
-        // autoFactory.bind("intake", stationIntake()).bind("score", scoring(ElevatorStates.L4));
         break;
       default:
         DriverStation.silenceJoystickConnectionWarning(true);
@@ -94,7 +88,6 @@ public class RobotContainer {
         //         new VisionIOSim(RealConstants.camConstants, drive::getPose));
         autoFactory =
             new AutoFactory(drive::getPose, drive::resetPose, drive::followChoreoPath, true, drive);
-        // autoFactory.bind("intake", stationIntake()).bind("score", scoring(ElevatorStates.L4));
         break;
     }
 
@@ -107,9 +100,13 @@ public class RobotContainer {
     RobotModeTriggers.autonomous().whileTrue(autoChooser.selectedCommandScheduler());
 
     // Add auto routines
+    // COMPETITION
     autoChooser.addRoutine("rightAutoRoutine", autoRoutines::rightAutoRoutine);
     autoChooser.addRoutine("taxiAutoRoutine", autoRoutines::taxiAutoRoutine);
     autoChooser.addRoutine("leftAutoRoutine", autoRoutines::leftAutoRoutine);
+
+    // TESTING ONLY
+    autoChooser.addRoutine("testingAutoRoutine", autoRoutines::testingAutoRoutine);
 
     // Logging setup
     // DataLogManager.start();
@@ -123,9 +120,11 @@ public class RobotContainer {
 
     configureBindings();
   }
-  private void configureBindings(){
+
+  private void configureBindings() {
     superstructureCommands.configureBindings();
   }
+
   public Command getAutonomousCommand() {
     return autoChooser.selectedCommandScheduler();
   }

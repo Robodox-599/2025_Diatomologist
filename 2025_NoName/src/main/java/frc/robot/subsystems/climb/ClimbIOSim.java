58a0a1ea5ca58baf639f8aa9a8e4d93e36b1a5d0
litlizlovesprog <lizzzz87658765@gmail.com>
2025-06-12 17:04:00 -0700

    Rewriting climb
    Started implementing the state machine to climb. I'm not done yet, I still need to do sim.

--- 2025_NoName/src/main/java/frc/robot/subsystems/climb/ClimbIOSim.java
+++ 2025_NoName/src/main/java/frc/robot/subsystems/climb/ClimbIOSim.java
@@ -1,7 +1,3 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems.climb;
 
 import dev.doglog.DogLog;
@@ -31,7 +27,7 @@ public class ClimbIOSim extends ClimbIO {
 
     positionController =
         new PIDController(ClimbConstants.simkP, ClimbConstants.simkI, ClimbConstants.simkD);
-    positionController.setTolerance(ClimbConstants.positionToleranceInches);
+    // positionController.setTolerance(ClimbConstants.positionToleranceInches);
   }
 
   @Override
@@ -39,47 +35,34 @@ public class ClimbIOSim extends ClimbIO {
     climbSim.update(0.02);
 
     // Update inputs structure
-    super.positionInches = climbSim.getAngularPositionRad() * ClimbConstants.inchesPerRev;
-    super.velocityInchesPerSec =
-        climbSim.getAngularAccelerationRadPerSecSq() * ClimbConstants.inchesPerRev;
-    super.appliedVolts = climbSim.getCurrentDrawAmps();
-    super.currentAmps = climbSim.getCurrentDrawAmps();
-    super.targetPositionInches = targetPositionInches;
-    super.tempCelsius = 25.0; // setting
+    // super.climbPositionInches = climbSim.getAngularPositionRad() * ClimbConstants.inchesPerRev;
+    // super.velocityInchesPerSec =
+    //     climbSim.getAngularAccelerationRadPerSecSq() * ClimbConstants.inchesPerRev;
+    // super.appliedVolts = climbSim.getCurrentDrawAmps();
+    // super.currentAmps = climbSim.getCurrentDrawAmps();
+    // super.targetPositionInches = targetPositionInches;
+    // super.tempCelsius = 25.0; // setting
 
-    climbSim.setInputVoltage(
-        positionController.calculate(super.positionInches, super.targetPositionInches));
+    // climbSim.setInputVoltage(
+    //     positionController.calculate(super.positionInches, super.targetPositionInches));
 
-    /* Checks if elevator is at setpoint */
-    super.atSetpoint = positionController.atSetpoint();
+    // /* Checks if elevator is at setpoint */
+    // super.atSetpoint = positionController.atSetpoint();
 
     // Update state
-    DogLog.log("Climb/PositionInches", super.positionInches);
-    DogLog.log("Climb/VelocityInchesPerSecond", super.velocityInchesPerSec);
-    DogLog.log("Climb/CurrentAmps", super.currentAmps);
-    DogLog.log("Climb/AppliedVoltage", super.appliedVolts);
+    // DogLog.log("Climb/PositionInches", super.positionInches);
+    // DogLog.log("Climb/VelocityInchesPerSecond", super.velocityInchesPerSec);
+    // DogLog.log("Climb/CurrentAmps", super.currentAmps);
+    // DogLog.log("Climb/AppliedVoltage", super.appliedVolts);
 
-    DogLog.log("Climb/PositionInches", super.positionInches);
-    DogLog.log("Climb/VelocityInchesPerSec", super.velocityInchesPerSec);
-    DogLog.log("Climb/AppliedVolts", super.appliedVolts);
-    DogLog.log("Climb/TargetPositionInches", super.targetPositionInches);
-    DogLog.log("Climb/AtSetpoint", super.atSetpoint);
-    DogLog.log("Climb/State", super.state.toString());
+    // DogLog.log("Climb/PositionInches", super.positionInches);
+    // DogLog.log("Climb/VelocityInchesPerSec", super.velocityInchesPerSec);
+    // DogLog.log("Climb/AppliedVolts", super.appliedVolts);
+    // DogLog.log("Climb/TargetPositionInches", super.targetPositionInches);
+    // DogLog.log("Climb/AtSetpoint", super.atSetpoint);
+    // DogLog.log("Climb/State", super.state.toString());
 
     // System.out.println(super.positionInches);
-  }
-
-  @Override
-  public void setState(ClimbConstants.ClimbStates state) {
-    super.state = state;
-
-    targetPositionInches =
-        MathUtil.clamp(
-            SubsystemUtil.climbStateToHeight(state),
-            ClimbConstants.climbLowerLimit,
-            ClimbConstants.climbUpperLimit);
-
-    climbSim.setInputVoltage(simPidController.calculate(targetPositionInches));
   }
 
   @Override
@@ -90,15 +73,5 @@ public class ClimbIOSim extends ClimbIO {
   @Override
   public void setVoltage(double voltage) {
     climbSim.setInputVoltage(voltage);
-  }
-
-  @Override
-  public void zeroEncoder() {
-    climbSim.setAngle(0);
-  }
-
-  @Override
-  public double getPositionInches() {
-    return climbSim.getAngularPositionRad() * ClimbConstants.inchesPerRev;
   }
 }
--- 2025_NoName/src/main/java/frc/robot/subsystems/climb/ClimbIOTalonFX.java
+++ 2025_NoName/src/main/java/frc/robot/subsystems/climb/ClimbIOTalonFX.java
@@ -1,97 +1,148 @@
 package frc.robot.subsystems.climb;
 
+import static frc.robot.subsystems.endefector.endefectorrollers.RollersConstants.gearRatio;
+
+import com.ctre.phoenix6.BaseStatusSignal;
+import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
 import dev.doglog.DogLog;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.BangBangController;
-import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Temperature;
+import edu.wpi.first.units.measure.Voltage;
+import frc.robot.subsystems.climb.ClimbConstants.ClimbStates;
 import frc.robot.util.PhoenixUtil;
 import frc.robot.util.SubsystemUtil;
 
 public class ClimbIOTalonFX extends ClimbIO {
 
-  private final TalonFX leaderMotor;
-  private final DigitalInput limitSwitch;
-  private ClimbConstants.ClimbStates currentState = ClimbConstants.ClimbStates.CLIMBREADY;
-
+  private final TalonFX climbMotor;
+  private final TalonFX rollersMotor;
   private final BangBangController bangBangController;
 
+   private final StatusSignal<Angle> climbPosition;
+   private final StatusSignal<AngularVelocity> climbVelocity;
+   private final StatusSignal<Voltage> climbAppliedVolts;
+   private final StatusSignal<Current> climbCurrent;
+   private final StatusSignal<Temperature> climbTemperature;
+
+   private final StatusSignal<AngularVelocity> rollersVelocity;
+   private final StatusSignal<Voltage> rollersAppliedVolts;
+   private final StatusSignal<Current> rollersCurrent;
+   private final StatusSignal<Temperature> rollersTemperature;
+   private final StatusSignal<Current> rollersStatorCurrent;
+
   public ClimbIOTalonFX() {
-    leaderMotor = new TalonFX(ClimbConstants.leaderMotorID, ClimbConstants.leaderMotorCANbus);
+    climbMotor = new TalonFX(ClimbConstants.climbMotorID, ClimbConstants.climbMotorCANbus);
+    rollersMotor = new TalonFX(ClimbConstants.rollersMotorID, ClimbConstants.rollersMotorCANbus);
     bangBangController = new BangBangController();
+    TalonFXConfiguration climbConfig = new TalonFXConfiguration();
+    TalonFXConfiguration rollersConfig = new TalonFXConfiguration();
 
-    /*  This tells the motor encoder where 0 inches is*/
-    limitSwitch = new DigitalInput(ClimbConstants.limitSwitchDioPort);
+    climbConfig.Slot0.kP = ClimbConstants.kP;
+    climbConfig.Slot0.kI = ClimbConstants.kI;
+    climbConfig.Slot0.kD = ClimbConstants.kD;
+    climbConfig.Slot0.kV = ClimbConstants.kV;
+    climbConfig.Slot0.kS = ClimbConstants.kS;
+    
+    climbConfig.CurrentLimits.SupplyCurrentLimit = 3;
+    climbConfig.CurrentLimits.StatorCurrentLimit = 3;
+    climbConfig.CurrentLimits.StatorCurrentLimitEnable = true;
+    climbConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
+    climbConfig.Feedback.RotorToSensorRatio = gearRatio;
+    rollersConfig.CurrentLimits.SupplyCurrentLimit = 3;
+    rollersConfig.CurrentLimits.StatorCurrentLimit = 3;
+    rollersConfig.CurrentLimits.StatorCurrentLimitEnable = true;
+    rollersConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
+    rollersConfig.MotorOutput.NeutralMode = NeutralModeValue.Brake;
 
-    TalonFXConfiguration config = new TalonFXConfiguration();
+    PhoenixUtil.tryUntilOk(5, () -> climbMotor.getConfigurator().apply(climbConfig, 0.25));
+    PhoenixUtil.tryUntilOk(5, () -> rollersMotor.getConfigurator().apply(rollersConfig, 0.25));
+    PhoenixUtil.tryUntilOk(5, () -> climbMotor.setPosition(0.0, 0.25));
+    climbMotor.optimizeBusUtilization();
+    rollersMotor.optimizeBusUtilization();
 
-    config.Slot0.kP = ClimbConstants.kP;
-    config.Slot0.kI = ClimbConstants.kI;
-    config.Slot0.kD = ClimbConstants.kD;
-    config.Slot0.kV = ClimbConstants.kV;
-    config.Slot0.kS = ClimbConstants.kS;
+    climbPosition = climbMotor.getPosition();
+    climbVelocity = climbMotor.getVelocity();
+    climbAppliedVolts = climbMotor.getMotorVoltage();
+    climbCurrent = climbMotor.getStatorCurrent();
+    climbTemperature = climbMotor.getDeviceTemp();
+    rollersVelocity = rollersMotor.getVelocity();
+    rollersAppliedVolts = rollersMotor.getMotorVoltage();
+    rollersCurrent = rollersMotor.getStatorCurrent();
+    rollersTemperature = rollersMotor.getDeviceTemp();
+    rollersStatorCurrent = rollersMotor.getStatorCurrent();
 
-    config.CurrentLimits.StatorCurrentLimit = ClimbConstants.statorCurrentLimitAmps;
-    config.CurrentLimits.StatorCurrentLimitEnable = true;
-    /* Helps prevent brown outs by limiting current spikes from the battery */
-    config.CurrentLimits.SupplyCurrentLimit = ClimbConstants.supplyCurrentLimitAmps;
-    config.CurrentLimits.SupplyCurrentLimitEnable = true;
-    // enableBrakeMode(true);
-    PhoenixUtil.tryUntilOk(5, () -> leaderMotor.getConfigurator().apply(config, 0.25));
-    PhoenixUtil.tryUntilOk(5, () -> leaderMotor.setPosition(0.0, 0.25));
-    leaderMotor.optimizeBusUtilization();
+    BaseStatusSignal.setUpdateFrequencyForAll(
+        50.0, climbTemperature, climbVelocity, climbPosition, climbCurrent, climbAppliedVolts,  
+        rollersTemperature, rollersVelocity, rollersCurrent, rollersAppliedVolts);
   }
 
   @Override
   public void updateInputs() {
-    super.positionInches =
-        leaderMotor.getPosition().getValueAsDouble() * ClimbConstants.inchesPerRev;
-    super.velocityInchesPerSec =
-        leaderMotor.getVelocity().getValueAsDouble() * ClimbConstants.inchesPerRev;
-    super.appliedVolts = leaderMotor.getMotorVoltage().getValueAsDouble();
-    super.currentAmps = leaderMotor.getSupplyCurrent().getValueAsDouble();
-    // super.targetPositionInches = motionMagicRequest.Position * ClimbConstants.inchesPerRev;
-    super.tempCelsius = leaderMotor.getDeviceTemp().getValueAsDouble();
-    super.state = currentState;
-    /* Determines if the elevator is at a setpoint */
-    double positionError = Math.abs(super.targetPositionInches - super.positionInches);
-    double velocityError = Math.abs(super.velocityInchesPerSec);
-    super.atSetpoint =
-        positionError < ClimbConstants.positionToleranceInches
-            && velocityError < ClimbConstants.velocityToleranceInchesPerSec;
-
-    super.limitSwitchValue = limitSwitch.get();
-    // Leader motor
-    DogLog.log("ClimbLeader/StatorCurrentAmps", super.currentAmps);
-    DogLog.log("ClimbLeader/AppliedVoltage", super.appliedVolts);
-    DogLog.log("ClimbLeader/TempCelcius", super.tempCelsius);
-
-    /* Log all super */
-    DogLog.log("Climb/TargetPositionInches", super.targetPositionInches);
-    DogLog.log("Climb/AtSetpoint", super.atSetpoint);
-    DogLog.log("Climb/State", super.state.toString());
-    DogLog.log("Climb/LimitSwitchValue", super.limitSwitchValue);
-    DogLog.log("Climb/PositionInches", super.positionInches);
-    DogLog.log("Climb/VelocityInchesPerSec", super.velocityInchesPerSec);
+    BaseStatusSignal.refreshAll(
+      climbTemperature, climbVelocity, climbPosition, climbCurrent, climbAppliedVolts,  
+      rollersTemperature, rollersVelocity, rollersCurrent, rollersAppliedVolts);
+      
+      super.climbAppliedVolts = climbAppliedVolts.getValueAsDouble();
+      super.climbCurrentAmps = climbCurrent.getValueAsDouble();
+      super.climbVelocity = climbVelocity.getValueAsDouble();
+      super.climbPositionDegrees = climbPosition.getValueAsDouble();
+      super.climbTempCelsius = climbTemperature.getValueAsDouble();
+      super.rollersAppliedVolts = rollersAppliedVolts.getValueAsDouble();
+      super.rollersCurrentAmps = rollersCurrent.getValueAsDouble();
+      super.rollersVelocity = rollersVelocity.getValueAsDouble();
+      super.rollersTempCelsius = rollersTemperature.getValueAsDouble();
+      super.rollersStatorCurrent = rollersStatorCurrent.getValueAsDouble();
+      super.isCageDetected = super.rollersStatorCurrent >= 20;
+    // super.atSetpoint =
+    //     positionError < ClimbConstants.positionToleranceInches
+    //         && velocityError < ClimbConstants.velocityToleranceInchesPerSec;
+ 
+    DogLog.log("Climb/IsCageDetected", super.isCageDetected);
+    DogLog.log("Climb/StatorCurrentAmps", super.climbCurrentAmps);
+    DogLog.log("Climb/AppliedVoltage", super.climbAppliedVolts);
+    DogLog.log("Climb/Velocity", super.climbVelocity);
+    DogLog.log("Climb/Temperature", super.climbTempCelsius);
+    DogLog.log("Climb/CurrentPosition", super.climbPositionDegrees);
+    DogLog.log("Climb/TargetPositon", super.targetPositionDegrees);
+    DogLog.log("Climb/Rollers/StatorCurrentAmps", super.rollersCurrentAmps);
+    DogLog.log("Climb/Rollers/AppliedVoltage", super.rollersAppliedVolts);
+    DogLog.log("Climb/Rollers/Velocity", super.rollersVelocity);
+    DogLog.log("Climb/Rollers/Temperature", super.rollersTempCelsius);
   }
 
   @Override
-  public void setState(ClimbConstants.ClimbStates state) {
-    currentState = state;
-    double targetPositionInches =
+  public void setClimb(ClimbStates state) {
+    double targetPositionDegrees =
         MathUtil.clamp(
             SubsystemUtil.climbStateToHeight(state),
             ClimbConstants.climbLowerLimit,
             ClimbConstants.climbUpperLimit);
+    climbMotor.setPosition(bangBangController.calculate(targetPositionDegrees));
+  }
 
-    leaderMotor.set(bangBangController.calculate(targetPositionInches));
+  @Override
+  public void setRollers(double velocity) {
+    rollersMotor.set(velocity);
+  }
+
+  @Override
+  public void setVoltage(double voltage) {
+    climbMotor.setControl(new VoltageOut(voltage));
   }
 
   @Override
   public void stop() {
-    leaderMotor.stopMotor();
+    climbMotor.stopMotor();
+    rollersMotor.stopMotor();
   }
 
   @Override
@@ -99,24 +150,9 @@ public class ClimbIOTalonFX extends ClimbIO {
     PhoenixUtil.tryUntilOk(
         5,
         () ->
-            leaderMotor.setNeutralMode(
+            climbMotor.setNeutralMode(
                 enable
                     ? com.ctre.phoenix6.signals.NeutralModeValue.Brake
                     : com.ctre.phoenix6.signals.NeutralModeValue.Coast));
-  }
-
-  @Override
-  public void setVoltage(double voltage) {
-    leaderMotor.setControl(new VoltageOut(voltage));
-  }
-
-  @Override
-  public void zeroEncoder() {
-    leaderMotor.setPosition(0);
-  }
-
-  @Override
-  public double getPositionInches() {
-    return leaderMotor.getPosition().getValueAsDouble();
   }
 }

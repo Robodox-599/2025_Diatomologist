58496028d67bbc8ccd86bfe0684e8d4f26b19670
jaiden <jaidenyummy123@gmail.com>
2025-10-09 20:48:17 -0700

    safety checker for l4

--- 2025_NoName/src/main/java/frc/robot/subsystems/elevator/Elevator.java
+++ 2025_NoName/src/main/java/frc/robot/subsystems/elevator/Elevator.java
@@ -68,6 +68,13 @@ public class Elevator {
 
   private void handleStateTransitions() {
     previousState = currentState;
+    if (currentState == CurrentState.POSITION_CORAL_L4
+        && wantedState != WantedState.POSITION_CORAL_L4) {
+      if (!subsystemChecker.isAtWristSetpoint(WristStates.POSITION_PREPARED)) {
+        currentState = CurrentState.POSITION_CORAL_L4;
+        return;
+      }
+    }
     switch (wantedState) {
       case INTAKING_CORAL_STATION:
         currentState = CurrentState.INTAKING_CORAL_STATION;
